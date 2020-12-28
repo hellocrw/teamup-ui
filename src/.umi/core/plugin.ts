285f279fb85449ddbd1529f25fc8ad13b4c23100
hellocrw <2388092655@qq.com>
2020-12-29 00:33:16 +0800

    init

---
+++ src/.umi/core/plugin.ts
@@ -0,0 +1,8 @@
+// @ts-nocheck
+import { Plugin } from 'C:/Users/crw/AppData/Local/Yarn/Data/global/node_modules/@umijs/runtime';
+
+const plugin = new Plugin({
+  validKeys: ['patchRoutes','rootContainer','render','onRouteChange',],
+});
+
+export { plugin };
